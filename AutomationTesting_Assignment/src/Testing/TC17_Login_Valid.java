0e50010a7946105ccf87b41e9e647f2483b7c16e
Riham Taha <rehamtaha35@gmail.com>
2020-06-08 16:12:40 +0200

    Login_ValidData

---
+++ AutomationTesting_Assignment/src/Testing/TC17_Login_Valid.java
@@ -0,0 +1,28 @@
+package Testing;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC17_Login_Valid {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "F:\\Chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.phptravels.net/login");	
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(50000, TimeUnit.SECONDS);
+
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("username")).sendKeys("Khaled_9636@gmail.com");
+		
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("Cooker");
+		driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/button")).click();
+
+	}
+
+}
